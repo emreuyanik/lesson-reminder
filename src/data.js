3f354516037da58f52f806e853e7e427d6aa18f5
Emre <144052970+emreuyanik@users.noreply.github.com>
2023-12-06 15:46:21 +0300

    change

--- src/data.js
+++ src/data.js
@@ -1,45 +1,42 @@
-export const data= [
+export const data = [
   {
     id: 1,
-    name: 'HTML',
+    name: "HTML",
     hour: "10:00 am",
-    image:
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonape.com%2Fhtml-5-2-logo-icon-svg-png"
+    image: "https://cdn.icon-icons.com/icons2/17/PNG/256/social_html5_1970.png",
   },
   {
     id: 2,
-    name: 'CSS',
-    hour:" 11:15 am",
+    name: "CSS",
+    hour: " 11:15 am",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png',
+      "https://images-ext-1.discordapp.net/external/0g7HyQuYRicNe8eRgvrLI60TfMxMwQIcr4hbDTm8tBY/https/play-lh.googleusercontent.com/RTAZb9E639F4JBcuBRTPEk9_92I-kaKgBMw4LFxTGhdCQeqWukXh74rTngbQpBVGxqo?format=webp",
   },
   {
     id: 3,
-    name: 'JAVASCRIPT',
+    name: "JAVASCRIPT",
     hour: "13:00 pm",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+      "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
   },
   {
     id: 4,
-    name: 'REACT',
+    name: "REACT",
     hour: "14:15 pm",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
   },
   {
     id: 5,
-    name: 'BOOTSTRAP',
+    name: "BOOTSTRAP",
     hour: "16:15 pm",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png',
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png",
   },
   {
     id: 6,
-    name: 'MUI',
+    name: "MUI",
     hour: "16:15 pm",
-    image:
-      'https://mui.com/static/logo.png',
+    image: "https://mui.com/static/logo.png",
   },
-
 ];
